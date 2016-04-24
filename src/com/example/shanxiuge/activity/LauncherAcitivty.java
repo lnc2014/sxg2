02abaeb488be0f388736a7a9f9f782964c1abcaa
zt <445194552@qq.com>
2016-04-24 13:46:21 +0800

    测试

--- src/com/example/shanxiuge/activity/LauncherAcitivty.java
+++ src/com/example/shanxiuge/activity/LauncherAcitivty.java
@@ -11,7 +11,12 @@ public class LauncherAcitivty extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_launcher);
-
+ 
 	}
+	
+	
+	
+	
+	
 
 }
